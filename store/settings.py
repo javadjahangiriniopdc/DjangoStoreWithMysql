ea0026efc2051fdd122b2d0f410a1ed761834ea8
drjj007 <javad.jahangiri.niopdc@gmail.com>
2021-02-25 15:06:55 +0330

    add login and logout page

--- store/settings.py
+++ store/settings.py
@@ -35,8 +35,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'orderapp'
-    , 'ckeditor'
+    'orderapp',
+    'ckeditor',
+    'accounts.apps.AccountsConfig'
 ]
 
 MIDDLEWARE = [
--- store/urls.py
+++ store/urls.py
@@ -20,6 +20,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('', include('orderapp.urls')),
-    path('orderapp/', include('orderapp.urls'))
+    path('orderapp/', include('orderapp.urls')),
+    path('accounts/', include('accounts.urls')),
 
 ]
