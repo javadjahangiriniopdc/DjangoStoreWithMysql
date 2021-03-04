a6f58c882d13c545a85043727fbf17ba880d0d1c
drjj007 <javad.jahangiri.niopdc@gmail.com>
2021-03-04 22:44:46 +0330

    fix verbose_name in Orderapp

--- orderapp/apps.py
+++ orderapp/apps.py
@@ -3,6 +3,6 @@ from django.apps import AppConfig
 
 class OrderappConfig(AppConfig):
     name = 'orderapp'
-
-    class Meta:
-        Verbose_name = 'برنامه مدیریت فروشگاه'
+    verbose_name = 'برنامه مدیریت فروشکاه'
+    verbose_name_plural = 'مدیریت فروشکاه ها'
+    app_label = 'مدیریت فروشگاه'
